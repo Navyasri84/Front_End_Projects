c93034c73c52be0cd06758aa20514fb1de967c94
Navyasri84 <116783041+Navyasri84@users.noreply.github.com>
2024-03-15 20:56:43 +0530

    Add files via upload

---
+++ JS_Projects/script.js
@@ -0,0 +1,26 @@
+let r = document.querySelector(":root");
+
+function violet() {
+    r.style.setProperty('--green', 'blueviolet');
+}
+function hotpink() {
+    r.style.setProperty('--green', 'hotpink');
+}
+function blue() {
+    r.style.setProperty('--green', 'blue');
+}
+function red() {
+    r.style.setProperty('--green', 'rgb(225, 12, 12)');
+}
+function purple() {
+    r.style.setProperty('--green', 'purple');
+}
+function yellow() {
+    r.style.setProperty('--green', 'rgb(206, 180, 9)');
+}
+function black() {
+    r.style.setProperty('--green', 'black');
+}
+function green() {
+    r.style.setProperty('--green', 'green');
+}
